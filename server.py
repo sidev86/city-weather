481b1b0e269381a176088601a9ebe421d6a543db
Samir Ibrahim <samir.ibrahim@outlook.it>
2024-07-27 15:16:47 +0200

    italian version

--- server.py
+++ server.py
@@ -2,6 +2,30 @@ from flask import Flask, render_template, request
 from weather import get_current_weather
 from waitress import serve
 
+def translate_weather_state(weather_data):
+    weather = weather_data["weather"][0]["description"].lower()
+    if weather == "few clouds":
+        status  = "poco nuvoloso"
+    elif weather == "scattered clouds":
+        status = "nuvole sparse"
+    elif weather == "broken clouds":
+        status = "nuvoloso"
+    elif weather == "shower rain":
+        status = "pioggia a rovesci"
+    elif weather == "thunderstorm":
+        status = "temporale"
+    elif weather == "snow":
+        status = "neve"
+    elif weather == "mist":
+        status = "nebbia"
+    elif weather == "clear sky":
+        status = "soleggiato"
+    elif weather == "rain":
+        status = "pioggia"
+    else:
+        status = weather
+    return status.capitalize();
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,6 +42,7 @@ def get_weather():
     weather_data = get_current_weather(city)
     if not weather_data["cod"] == 200:
         return render_template('city-not-found.html')
-    return render_template('weather.html', title=weather_data["name"], status=weather_data["weather"][0]["description"].capitalize(), temp=f"{weather_data["main"]["temp"]:.1f}", feels_like=f"{weather_data["main"]["feels_like"]:.1f}")
+    weather = translate_weather_state(weather_data)
+    return render_template('weather.html', title=weather_data["name"], status = weather, temp=f"{weather_data["main"]["temp"]:.1f}", feels_like=f"{weather_data["main"]["feels_like"]:.1f}")
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
